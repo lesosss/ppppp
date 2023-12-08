fabad0f574e06a19dec857a2394bf6d0dd1e8541
yelenanikitina1625@hotmail.com <yelenanikitina1625@hotmail.com>
2023-12-08 15:41:30 +0500

    avia

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(105, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ЖДКасса";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(33, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 165);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(394, 271);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Покупка посадочных билетов";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
