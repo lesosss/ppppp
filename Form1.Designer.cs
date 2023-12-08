8f37a226787304fac1f180b692e0ad3e179c952a
yelenanikitina1625@hotmail.com <yelenanikitina1625@hotmail.com>
2023-12-08 14:15:43 +0500

    pr1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,308 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel33 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel22 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnPermutations = new System.Windows.Forms.RadioButton();
+            this.BtnPlacement = new System.Windows.Forms.RadioButton();
+            this.BtnСombination = new System.Windows.Forms.RadioButton();
+            this.panel33.SuspendLayout();
+            this.panel22.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.OldLace;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(487, 438);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 31);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Ответ:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Linen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(306, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 46);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Результат";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel33
+            // 
+            this.panel33.BackColor = System.Drawing.Color.Bisque;
+            this.panel33.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel33.Controls.Add(this.textBox5);
+            this.panel33.Controls.Add(this.textBox6);
+            this.panel33.Controls.Add(this.label6);
+            this.panel33.Controls.Add(this.label7);
+            this.panel33.Location = new System.Drawing.Point(521, 193);
+            this.panel33.Name = "panel33";
+            this.panel33.Size = new System.Drawing.Size(188, 202);
+            this.panel33.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.Location = new System.Drawing.Point(27, 156);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(126, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox6.Location = new System.Drawing.Point(27, 64);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(126, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(14, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Введите значение n";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(14, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(164, 20);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Введите значение n";
+            // 
+            // panel22
+            // 
+            this.panel22.BackColor = System.Drawing.Color.Bisque;
+            this.panel22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel22.Controls.Add(this.textBox3);
+            this.panel22.Controls.Add(this.textBox4);
+            this.panel22.Controls.Add(this.label4);
+            this.panel22.Controls.Add(this.label5);
+            this.panel22.Location = new System.Drawing.Point(285, 193);
+            this.panel22.Name = "panel22";
+            this.panel22.Size = new System.Drawing.Size(188, 202);
+            this.panel22.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.Location = new System.Drawing.Point(27, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(126, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Location = new System.Drawing.Point(27, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(126, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(14, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Введите значение n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(14, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Введите значение m";
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.Bisque;
+            this.panel11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel11.Controls.Add(this.textBox2);
+            this.panel11.Controls.Add(this.textBox1);
+            this.panel11.Controls.Add(this.label3);
+            this.panel11.Controls.Add(this.label2);
+            this.panel11.Location = new System.Drawing.Point(64, 193);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(188, 202);
+            this.panel11.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(31, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(31, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(11, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите значение n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(11, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите значение m";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(182, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(426, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Выберете элемент комбинаторики";
+            // 
+            // BtnPermutations
+            // 
+            this.BtnPermutations.AutoSize = true;
+            this.BtnPermutations.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnPermutations.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnPermutations.Location = new System.Drawing.Point(515, 123);
+            this.BtnPermutations.Name = "BtnPermutations";
+            this.BtnPermutations.Size = new System.Drawing.Size(194, 33);
+            this.BtnPermutations.TabIndex = 11;
+            this.BtnPermutations.TabStop = true;
+            this.BtnPermutations.Text = "Перестановка";
+            this.BtnPermutations.UseVisualStyleBackColor = true;
+            this.BtnPermutations.CheckedChanged += new System.EventHandler(this.BtnPermutations_CheckedChanged);
+            // 
+            // BtnPlacement
+            // 
+            this.BtnPlacement.AutoSize = true;
+            this.BtnPlacement.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnPlacement.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnPlacement.Location = new System.Drawing.Point(285, 123);
+            this.BtnPlacement.Name = "BtnPlacement";
+            this.BtnPlacement.Size = new System.Drawing.Size(183, 33);
+            this.BtnPlacement.TabIndex = 10;
+            this.BtnPlacement.TabStop = true;
+            this.BtnPlacement.Text = "Размещение";
+            this.BtnPlacement.UseVisualStyleBackColor = true;
+            this.BtnPlacement.CheckedChanged += new System.EventHandler(this.BtnPlacement_CheckedChanged);
+            // 
+            // BtnСombination
+            // 
+            this.BtnСombination.AutoSize = true;
+            this.BtnСombination.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnСombination.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnСombination.Location = new System.Drawing.Point(80, 123);
+            this.BtnСombination.Name = "BtnСombination";
+            this.BtnСombination.Size = new System.Drawing.Size(159, 33);
+            this.BtnСombination.TabIndex = 9;
+            this.BtnСombination.TabStop = true;
+            this.BtnСombination.Text = "Сочетание";
+            this.BtnСombination.UseVisualStyleBackColor = true;
+            this.BtnСombination.CheckedChanged += new System.EventHandler(this.BtnСombination_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(790, 507);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel33);
+            this.Controls.Add(this.panel22);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnPermutations);
+            this.Controls.Add(this.BtnPlacement);
+            this.Controls.Add(this.BtnСombination);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel33.ResumeLayout(false);
+            this.panel33.PerformLayout();
+            this.panel22.ResumeLayout(false);
+            this.panel22.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel33;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel22;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton BtnPermutations;
+        private System.Windows.Forms.RadioButton BtnPlacement;
+        private System.Windows.Forms.RadioButton BtnСombination;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,112 @@ using System.Windows.Forms;
 
 namespace ppppp
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnСombination_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BtnСombination.Checked)
+            {
+                panel11.Enabled = true;
+                panel22.Enabled = false;
+                panel33.Enabled = false;
+            }
+        }
+
+        private void BtnPlacement_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BtnPlacement.Checked)
+            {
+                panel11.Enabled = false;
+                panel22.Enabled = true;
+                panel33.Enabled = false;
+            }
+        }
+
+        private void BtnPermutations_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BtnPermutations.Checked)
+            {
+                panel11.Enabled = false;
+                panel22.Enabled = false;
+                panel33.Enabled = true;
+            }
+        }
+
+        private int factarial(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * factarial(n - 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            int m;
+
+            if (BtnСombination.Checked)
+            {
+                if (!int.TryParse(textBox1.Text, out m) || !int.TryParse(textBox2.Text, out n))
+                {
+                    MessageBox.Show("Введите целое число не больше 20");
+                    return;
+                }
+
+
+                if (n > 20 || m > 20 || n < 0 || m < 0)
+                {
+                    MessageBox.Show("Введите целое число не больше 20");
+                    return;
+                }
+
+                label8.Text = ((factarial(n + m - 1) / (factarial(m) * factarial(n - 1))).ToString());
+            }
+
+            if (BtnPlacement.Checked)
+            {
+                if (!int.TryParse(textBox3.Text, out m) || !int.TryParse(textBox4.Text, out n))
+                {
+                    MessageBox.Show("Введите целое число не больше 20");
+                    return;
+                }
+
+
+                if (n > 20 || m > 20 || n < 0 || m < 0)
+                {
+                    MessageBox.Show("Введите целое число не больше 20");
+                    return;
+                }
+
+                label8.Text = Math.Pow(n, m).ToString();
+
+            }
+
+
+            if (BtnPermutations.Checked)
+            {
+                if (!int.TryParse(textBox5.Text, out m) || !int.TryParse(textBox6.Text, out n))
+                {
+                    MessageBox.Show("Введите целое число не больше 20");
+                    return;
+                }
+
+
+                if (n > 20 || m > 20 || n < 0 || m < 0)
+                {
+                    MessageBox.Show("Введите целое число не больше 20");
+                    return;
+                }
+
+                label8.Text = (factarial(n + m) / (factarial(n) * factarial(m))).ToString();
+
+            }
         }
     }
 }
